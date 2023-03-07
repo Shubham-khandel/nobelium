041e5e798bfcd1eaab6212cd4ad026da4866a016
Shubham <104565425+Shubham-khandel@users.noreply.github.com>
2023-03-07 22:01:39 +0530

    Update App2.js

--- src/App2.js
+++ src/App2.js
@@ -3,7 +3,7 @@ import { Btn, Heading, Input } from './Challenge2';
 
 function App2(){
     return(
-        <div className='Box'>
+        <div className='Box2'>
             <div className='App2'>
             <Heading/>
             <Input/>
