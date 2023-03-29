ee3924012aed215cfa284dda730ff978e36c1b8e
Shubham-Sharma <viku.vsk@gmail.com>
2023-03-29 22:28:47 +0530

    Quiz App

---
+++ Nb/quiz_app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Test from './Components/Test'
+import "./App.css";
+
+const App = () => {
+return (
+<div className='mainContainer'>
+    <div className='insideCont'>
+    <Test />
+    </div>
+</div>
+);
+};
+
+export default App;
